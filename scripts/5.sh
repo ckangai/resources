8ec4e3ec7a7a089cd1807dc645d2c47f36ac431a
Charles Kangai <charles.kangai@roitraining.com>
2025-07-05 19:44:15 +0100

    Change various script files

--- scripts/5.sh
+++ scripts/5.sh
@@ -46,21 +46,5 @@ echo "Simulating high memory usage surges..."
 done) &
 echo "Private VM memory pressure simulation started (PID $!)"
 
-if bq --project_id="${PROJECT_ID}" mk --schema=product_id:integer,product_name:string,supplier_id:integer,category_id:integer,quantity_per_unit:string,unit_price:float,units_in_stock:integer,units_on_order:integer,reorder_level:integer,discontinued:boolean --table "$1:demos.products" 2>&1 | logger -t "bq-table-creation"; then
-  echo "Successfully created BigQuery table $1:demos.products." | logger -t "bq-table-creation"
-else
-  echo "ERROR: Failed to create BigQuery table $1:demos.products. Check logs for details." | logger -t "bq-table-creation"
-fi
-
-bq --project_id="${PROJECT_ID}" query \
-   --use_legacy_sql=false \
-   --format=prettyjson \
-   "SELECT * FROM demos.products"
-bq mk --dataset $1:demos
-
-bq --project_id {SUB_PROJECT_ID} load --source_format=CSV \
---autodetect \
-demos.customers \
-gs://{SUB_PROJECT_ID}/customers.csv
 
 echo "Private VM simulation scripts are running in the background. Check logs for activity in Cloud Logging."
