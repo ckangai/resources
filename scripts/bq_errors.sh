02a9ca43146116142daf8befcca6a76feaabb20b
Charles Kangai <charles.kangai@roitraining.com>
2025-07-25 02:35:05 +0100

    Add line to retrieve project_id at start of bq_errors.sh file

--- scripts/bq_errors.sh
+++ scripts/bq_errors.sh
@@ -1,6 +1,7 @@
 # BigQuery error
 
 # --- BigQuery Errors Simulation ---
+PROJECT_ID=$(gcloud config get-value project)
 if bq --project_id="${PROJECT_ID}" mk --schema=product_id:integer,product_name:string,supplier_id:integer,category_id:integer,quantity_per_unit:string,unit_price:float,units_in_stock:integer,units_on_order:integer,reorder_level:integer,discontinued:boolean --table "$1:demos.products" 2>&1 | logger -t "bq-table-creation"; then
   echo "Successfully created BigQuery table ${PROJECT_ID}:demos.products." | logger -t "bq-table-creation"
 else
